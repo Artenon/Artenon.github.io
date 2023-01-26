851b7191b08f08560ce86e431f5b03d7ddf4f6b1
Artem <artem_1parygin_2@mail.ru>
2023-01-26 21:31:47 +0500

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+new Swiper('.swiper', {
+  /* direction: 'vertical' */
+  speed: 2000,
+  mousewheel: true,
+  spaceBetween: 20,
+  parallax: true,
+});
+
+Particles.init({
+  selector: '.particles',
+  color: '#BB695Eaa',
+  minDistance: 20,
+  sizeVariations: 3,
+});
