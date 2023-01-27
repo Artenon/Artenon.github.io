b8bd667cc27dccf5e5449612d94621a3968bad22
Artem <artem_1parygin_2@mail.ru>
2023-01-27 15:19:52 +0500

    add welcome modal

--- script.js
+++ script.js
@@ -4,6 +4,13 @@ new Swiper('.swiper', {
   mousewheel: true,
   spaceBetween: 20,
   parallax: true,
+  keyboard: true,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    dynamicBullets: true,
+    clickable: true
+  },
 });
 
 Particles.init({
@@ -11,4 +18,19 @@ Particles.init({
   color: '#BB695Eaa',
   minDistance: 20,
   sizeVariations: 3,
+});
+
+const modalContainer = document.querySelector('#modal-container');
+const body = document.querySelector('body');
+
+modalContainer.addEventListener('click', () => {
+  modalContainer.classList.add('out');
+  body.classList.remove('modal-active');
+});
+
+window.addEventListener('keypress', (e) => {
+  if (e.code === 'Space') {
+    modalContainer.classList.add('out');
+    body.classList.remove('modal-active');
+  }
 });
